2a3e004e77855e649b886c68ecc047f12555e3eb
robert2030 <robert7ashioya@gmail.com>
2024-10-08 16:54:25 +0300

    main.py

--- 0x01-lockboxes/main.py
+++ 0x01-lockboxes/main.py
@@ -4,3 +4,14 @@ Test the canUnlockAll function.
 """
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
+
+# Test cases
+boxes = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes))  # Expected: True
+
+boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes))  # Expected: True
+
+boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes))  # Expected: False
+
