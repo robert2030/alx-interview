d90645cc4bd0fcd5f82456434b7d482a09c929ef
robert2030 <robert7ashioya@gmail.com>
2024-10-08 16:47:55 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
 # lock boxes 
 def canUnlockAll(boxes):
-    """
-    Solution to the lockboxes problem
-    """
-    if (type(boxes)) is not list:
-        return False
-    elif (len(boxes)) == 0:
-        return False
-
-    for k in range(1, len(boxes) - 1):
-        boxes_seen = False
-        for idx in range(len(boxes)):
-            boxes_seen = k in boxes[idx] and k != idx
-            if boxes_seen:
-                break
-        if boxes_seen is False:
-            return boxes_seen
-    return True
+    n = len(boxes)
+    opened = set([0])  # Start with the first box opened
+    keys = [0]  # Start with the keys from the first box (box 0)
+    
+    while keys:
+        current_box = keys.pop()  # Take the last box to open
+        for key in boxes[current_box]:
+            if key not in opened and key < n:
+                opened.add(key)
+                keys.append(key)  # Add the keys from this new box to be opened
+                
+    return len(opened) == n
