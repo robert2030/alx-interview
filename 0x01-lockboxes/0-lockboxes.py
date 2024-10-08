100a511a913ca90d1d894ce6e49d3df98cc4e82e
robert2030 <robert7ashioya@gmail.com>
2024-10-08 16:50:45 +0300

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 # lock boxes 
+#!/usr/bin/python3
+"""
+Module to determine if all boxes can be unlocked.
+"""
+
 def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked.
+
+    Args:
+        boxes (list): A list of lists containing keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
     n = len(boxes)  # Number of boxes
     opened = set([0])  # Start with box 0 unlocked
-    stack = [0]  # Start exploring from box 0
-    
+    stack = [0]  # Stack for boxes to explore
+
     # Explore the boxes while there are keys to process
     while stack:
         box = stack.pop()  # Take the last box opened
