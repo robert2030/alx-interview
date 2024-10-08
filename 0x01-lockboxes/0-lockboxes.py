d580f839c2297c4fa98df7af49e25b617c16ee52
robert2030 <robert7ashioya@gmail.com>
2024-10-08 17:02:42 +0300

    lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,27 +6,25 @@ Module to determine if all boxes can be unlocked.
 """
 
 def canUnlockAll(boxes):
+    """Checks if all boxes can be opened
+
+        Args: boxes (List): List of Lists of integers
+
+        Returns:
+            (bool): Tells whether all items in the list can be opened
     """
-    Determines if all boxes can be unlocked.
+    listSize = len(boxes)
+    openedBoxes = [False] * listSize
 
-    Args:
-        boxes (list): A list of lists containing keys to other boxes.
+    stack = [0]
 
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
-    """
-    n = len(boxes)  # Number of boxes
-    opened = set([0])  # Start with box 0 unlocked
-    stack = [0]  # Stack for boxes to explore
-
-    # Explore the boxes while there are keys to process
     while stack:
-        box = stack.pop()  # Take the last box opened
-        for key in boxes[box]:
-            if key not in opened and key < n:  # Only unlock unopened boxes within bounds
-                opened.add(key)  # Mark the box as opened
-                stack.append(key)  # Add the new box to explore further
+        currentBox = stack.pop()
 
-    # Return True if all boxes are opened
-    return len(opened) == n
+        openedBoxes[currentBox] = True
+        currentKeys = boxes[currentBox]
+        for key in currentKeys:
+            if key in range(listSize) and openedBoxes[key] is False and key not in stack:
+                stack.append(key)
 
+    return all(openedBoxes)
